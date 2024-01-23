5c6fe2d41388989a377776004a10d44aacf74a9b
Yanxiang Zhan <yanxiang@uni-bremen.de>
2024-01-23 10:56:00 +0100

    set rvizweb proxy url to ros parameter

--- src/rosparam.ts
+++ src/rosparam.ts
@@ -2,14 +2,20 @@ import ROSLIB from 'roslib';
 
 export function setRosParamsFromUrl(baseUrl: string) {
   const ros = createRos(getWsUrl(baseUrl))
-
+  const jupyterProxyUrl = `${baseUrl}proxy/8001/rvizweb/webapps/rvizweb/build/www/index.html?baseurl=${baseUrl}`
+  new ROSLIB.Param({
+    ros : ros,
+    name : 'rvizweb/jupyter_proxy_url'
+  }).set(jupyterProxyUrl, function() {
+    console.log('Set ROS param: /rvizweb/jupyter_proxy_url to ' + jupyterProxyUrl)
+  });
   for (const [key, value] of new URLSearchParams(window.location.search).entries()) {
     const _param = new ROSLIB.Param({
       ros : ros,
       name : 'nbparam_' + key
     });
     _param.set(value, function() {
-      console.log('Set ros param:', key, value)
+      console.log(`Set ROS param '${key}' to '${value}`)
     });
   }
 }
